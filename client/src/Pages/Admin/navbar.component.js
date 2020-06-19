7f6690baa36198ab996ac8b3f503b2d64605cd9d
nirosshan-ran <inranasinghe2208@gmail.com>
2020-06-19 23:45:22 +0530

    Final Commit by Niroshan before Viva...

--- client/src/Pages/Admin/navbar.component.js
+++ client/src/Pages/Admin/navbar.component.js
@@ -11,9 +11,10 @@ export default class Navbar extends Component {
                             <li className="navbar-item">
                                 <Link to="/admin" className="nav-link">All Products</Link>
                             </li>
-                            <li className="navbar-item">
-                                <Link to="/category" className="nav-link">Create Categories</Link>
-                            </li>
+                            {localStorage.getItem("admin") ?
+                                <li className="navbar-item">
+                                    <Link to="/category" className="nav-link">Create Categories</Link>
+                                </li> : <></>}
                             <li className="navbar-item">
                                 <Link to="/product" className="nav-link">Create Products</Link>
                             </li>
